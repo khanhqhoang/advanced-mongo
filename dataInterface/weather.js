850bd830f248d714ae484e5172e42eed3c7c07a4
Khanh Hoang <khanhqhoang@hotmail.com>
2022-07-30 08:14:37 -0700

    Khanh's WK4 assginment interim checkin

--- dataInterface/weather.js
+++ dataInterface/weather.js
@@ -10,14 +10,127 @@ const databaseName = 'sample_weatherdata';
 const collName = 'data';
 
 module.exports = {}
-
+//getWeather by CallCenters
 // https://www.mongodb.com/docs/drivers/node/current/usage-examples/find/
 module.exports.getWeather = async (callLetterId) => {
   const database = client.db(databaseName);
   const weather = database.collection(collName);
 
-  const query = { callLetterId: ObjectId(movieId)};
-  let weatherCursor = await weather.find(query).limit(10);
+  const query = { callLetters: callLetterId};
+  let weatherCursor = await weather.find(query).limit(2);
 
   return weatherCursor.toArray();
+}
+//searchWeather by one or more params
+module.exports.searchWeather = async (queryObject) => {
+  const database = client.db(databaseName);
+  const weather = database.collection(collName);
+  let queryOpt
+  const options = { 
+    sort: { callLetters: 1},
+    projection: { _id: 1, callLetters: 1, sections: 1, airTemperature: 1, position: 1, pressure: 1, wind: 1 },
+    limit: 10
+  }
+
+ 
+  try
+  {
+     //validate queryObject
+    queryOpt = validateQueryObject(queryObject);
+    console.log(queryOpt);
+    let weatherList = await weather.find(queryOpt, options);
+
+    return weatherList
+    ? weatherList.toArray()
+    : {
+        error: `We've encountered an error. Please try again later.`,
+      };
+  }
+  catch (err)
+  {
+    return { error: err};
+  }
+  
+}
+
+//validate queryObject
+function validateQueryObject(qryobj)
+{
+  let query
+  if (qryobj.callLetters)
+  {
+    query = {...query, callLetters: qryobj.callLetters};
+  }
+  if (qryobj.section)
+  {
+    query = {...query, sections: [qryobj.section] };
+  }
+  // if both minAirTemp and maxAirTemp
+  if (qryobj.minAirTemp && qryobj.maxAirTemp)
+  {
+    // if both minAirTemp and maxAirTemp are valid numbers
+    if (!isNaN(qryobj.minAirTemp) && !isNaN(qryobj.maxAirTemp))
+    {
+      query = {...query, "airTemperature.value": { $gte: Number(qryobj.minAirTemp), $lte: Number(qryobj.maxAirTemp) } };
+    }
+    else
+      {
+        return {error: "Both minAirTemp and maxAirTemp must be valid numbers."};
+      }
+  }
+  else
+  {
+    //if minAirTemp
+    if (qryobj.minAirTemp)
+    {
+      //minAirTemp is a number
+      if (!isNaN(qryobj.minAirTemp))
+      {      
+        query = {...query, "airTemperature.value": { $gte: Number(qryobj.minAirTemp) }};
+      }
+      else
+      {
+        return {error: "minAirTemp must be a number."};
+      }
+    }
+    if (qryobj.maxAirTemp)
+    {
+      if (!isNaN(qryobj.maxAirTemp))
+      {
+        query = {...query, "airTemperature.value": {$lte: Number(qryobj.maxAirTemp) }};
+      }
+      else
+      {
+        return {error: "maxAirTemp must be a float."};
+      }
+    }
+    // if passed validation then return query string
+    return query;
+  }
+
+}
+//Create weather
+module.exports.create = async (newObj) => {
+  const database = client.db(databaseName);
+  const weather = database.collection(collName);
+
+  if(!newObj.callLetters){
+    // Invalid movie object, shouldn't go in database.
+    return {error: "Weather must have a callLetters."}
+  }
+  if(!newObj.airTemperature.value){
+    // Invalid movie object, shouldn't go in database.
+    return {error: "Weather must have airtemperature!"}
+  }
+  if(!newObj.sections.length === 0){
+    // Invalid movie object, shouldn't go in database.
+    return {error: "Weather must have sections!"}
+  }
+  const result = await weather.insertOne(newObj);
+
+  if(result.acknowledged){
+    return { newObjectId: result.insertedId, message: `Item created! ID: ${result.insertedId}` }
+  } else {
+    return {error: "Something went wrong. Please try again."}
+  }
 }
--- routes/weather.js
+++ routes/weather.js
@@ -1,19 +1,69 @@
 const { Router } = require("express");
 const router = Router();
+const bodyParser = require('body-parser');
 
 const weatherData = require("../dataInterface/weather");
-// get weather by CallLetter
-// curl http://localhost:5000/weather:callLetterId
-router.get("/", async (req, res, next) => {
-  console.log(req.query);
-  let weatherList = await weatherData.getWeather();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+// get weather by callLetters
+// "curl http://localhost:5000/weather/PLAT"
+router.get("/:callCenters", async (req, res, next) => {
+
+  let weatherList = await weatherData.getWeather(req.params.callCenters);
 
   if (weatherList) {
     res.status(200).send(weatherList);
   } else {
-    // If movieList is empty/null, something serious is wrong with the MongoDB connection.
+    // If weatherList is empty/null, something serious is wrong with the MongoDB connection.
     res.status(500).send({ error: "Something went wrong. Please try again." });
   }
 });
 
+//get weather by one or more params
+//curl "http://localhost:5000/weather?minAirTemp=5"
+//curl "http://localhost:5000/weather?minAirTemp=5&maxAirTemp=90"
+//curl "http://localhost:5000/weather?section=AG1"
+//curl "http://localhost:5000/weather?callLetters=VC81&minAirTemp=35"
+
+router.get("/", async (req, res, next) => {
+  let resultStatus
+  // Validate for a valid url query param
+  if (req.query)
+  {
+    let weatherList = await weatherData.searchWeather(req.query);
+
+    if (weatherList) {
+      resultStatus = 200
+      res.status(resultStatus).send(weatherList);
+    } 
+    else 
+    {
+      // If weatherList is empty/null, something serious is wrong with the MongoDB connection.
+      resultStatus = 500
+      res.status(resultStatus).send({ error: "Something went wrong. Please try again." });
+    }
+  }
+  else
+  {
+    // If no url param provided
+    res.status(500).send({ error: "No query param provided in url. Please try again." });
+  }
+  
+});
+//post a weather entry
+//  curl -X POST -H "Content-Type: application/json" -d '{"callLetters":"AAAA", "airTemperature":{"value": 65, "quality": "1"}, "sections":["LB1","LB2","LB3"]}' http://localhost:5000/weather
+router.post("/", async (req, res, next) => {
+  let resultStatus;
+  const result = await weatherData.create(req.body);
+
+  if(result.error){
+    resultStatus = 400;
+  } else {
+    resultStatus = 200;
+  }
+
+  res.status(resultStatus).send(result);
+});
 module.exports = router;
